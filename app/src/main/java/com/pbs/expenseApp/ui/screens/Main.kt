4fe1e18e551bf2cc62dd04391c6f057fbef05f3b
pbascunana <pablobascunanasaiz@gmail.com>
2023-12-10 18:04:57 +0100

    PR-3 Add main screen

---
+++ app/src/main/java/com/pbs/expenseApp/ui/screens/Main.kt
@@ -0,0 +1,21 @@
+package com.pbs.expenseApp.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun MainScreen(navHostController: NavHostController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Hello Main Screen!")
+    }
+}
