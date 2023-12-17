a30cf594f7c7977901b4505a04f4eef4abdf118a
pbascunana <pablobascunanasaiz@gmail.com>
2023-12-17 11:26:40 +0100

    PR-7 Add viewModel dependencies and add name application in manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,9 +51,11 @@ android {
 }
 
 dependencies {
+    val lifeCycleVersion = "2.6.2"
     val roomVersion = "2.5.0"
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifeCycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifeCycleVersion")
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
