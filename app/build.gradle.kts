39aa050ae0a5ba110a6995deb2bd14623bc06de9
Pablo Bascuñana <pablo.bascunana@innocv.com>
2023-12-19 08:48:34 +0100

    PR-14 Upgrade material3 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
     implementation("androidx.room:room-runtime:$roomVersion")
