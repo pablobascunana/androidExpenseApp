164b0e1cad7118ccdc174abbef1d3c9cd44ff617
Pablo Bascuñana <pablo.bascunana@innocv.com>
2023-12-19 08:49:35 +0100

    PR-14 Rename MyText component to AppText

--- app/src/main/java/com/pbs/expenseApp/ui/components/AppText.kt
+++ app/src/main/java/com/pbs/expenseApp/ui/components/AppText.kt
@@ -1,12 +1,12 @@
-package com.pbs.expenseApp.ui.composables
+package com.pbs.expenseApp.ui.components
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 
 @Composable
-fun MyText(id: Int) {
+fun AppText(id: Int) {
     Text(
-        text = stringResource(id = id)
+        text = stringResource(id = id),
     )
 }
