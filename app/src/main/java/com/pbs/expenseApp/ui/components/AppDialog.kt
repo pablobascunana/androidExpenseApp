6e55904f9c36d6b2ed59e0a8a936524ed9e52e03
Pablo Bascuñana <pablo.bascunana@innocv.com>
2023-12-24 12:35:54 +0100

    PR-17 Create AppDialog

---
+++ app/src/main/java/com/pbs/expenseApp/ui/components/AppDialog.kt
@@ -0,0 +1,44 @@
+package com.pbs.expenseApp.ui.components
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import com.pbs.expenseApp.R
+
+
+@Composable
+fun AppAlertDialog(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String,
+    dialogText: String,
+    icon: ImageVector,
+) {
+    AlertDialog(
+        icon = {
+            AppIcon(imageVector = icon, contentDescription = stringResource(id = R.string.delete))
+        },
+        title = { AppText(text = dialogTitle) },
+        text = { AppText(text = dialogText) },
+        onDismissRequest = {
+            onDismissRequest()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = { onConfirmation() }
+            ) {
+                AppText(text = stringResource(id = R.string.confirm))
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = { onDismissRequest() }
+            ) {
+                AppText(text = stringResource(id = R.string.cancel))
+            }
+        }
+    )
+}
