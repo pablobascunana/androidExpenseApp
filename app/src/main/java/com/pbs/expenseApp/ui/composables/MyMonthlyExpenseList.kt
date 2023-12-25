1131a668b80efcc15226e70832ed2b58d72cbea5
Pablo Bascuñana <pablo.bascunana@innocv.com>
2023-12-25 13:57:22 +0100

    PR-23 Add list and message for empty list

--- app/src/main/java/com/pbs/expenseApp/ui/composables/MyMonthlyExpenseList.kt
+++ app/src/main/java/com/pbs/expenseApp/ui/composables/MyMonthlyExpenseList.kt
@@ -1,40 +1,70 @@
 package com.pbs.expenseApp.ui.composables
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.pbs.expenseApp.R
 import com.pbs.expenseApp.domain.model.CategoryType
 import com.pbs.expenseApp.domain.model.Expense
 import com.pbs.expenseApp.navigation.AppRoutes
+import com.pbs.expenseApp.ui.AppViewModelProvider
+import com.pbs.expenseApp.ui.components.AppColumn
+import com.pbs.expenseApp.ui.components.AppList
 import com.pbs.expenseApp.ui.components.AppText
+import com.pbs.expenseApp.ui.viewmodels.ExpenseViewModel
 
 @Composable
 fun MyMonthlyExpenseList(
     navHostController: NavHostController
 ) {
+    val expenseVM: ExpenseViewModel = viewModel(factory = AppViewModelProvider.Factory)
+
     val currentRoute = navHostController.currentBackStackEntry?.destination?.route
 
     var text = stringResource(id = R.string.add_monthly_expenses)
     var type = CategoryType.EXPENSE.value
+    var listItems = expenseVM.expenseList
     if (currentRoute == AppRoutes.AddIncome.route) {
         text = stringResource(id = R.string.add_monthly_incomes)
         type = CategoryType.INCOME.value
+        listItems = expenseVM.incomeList
     }
-    AppText(
-        text = text,
-        fontSize = fontDimensionResource(id = R.dimen.font_md),
-        color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_sm))
-    )
-    val incomes = emptyList<List<Expense>>()
-    /* AppList(
-        incomes,
-        onEdit = {  },
-        onDelete = {  }
-    ) */
+
+    AppColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(dimensionResource(id = R.dimen.padding_sm))
+    ){
+        AppText(
+            text = text,
+            fontSize = fontDimensionResource(id = R.dimen.font_md),
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_sm))
+        )
+        if (listItems.isEmpty()) {
+            AppText(
+                text = stringResource(id = R.string.no_items_in_list),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentHeight(align = Alignment.CenterVertically),
+                textAlign = TextAlign.Center
+            )
+        } else {
+            AppList(
+                listItems,
+                onEdit = {  },
+                onDelete = {  }
+            )
+        }
+    }
 }
--- app/src/main/java/com/pbs/expenseApp/ui/viewmodels/ExpenseViewModel.kt
+++ app/src/main/java/com/pbs/expenseApp/ui/viewmodels/ExpenseViewModel.kt
@@ -4,8 +4,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.pbs.expenseApp.domain.model.Expense
 
 class ExpenseViewModel: ViewModel() {
 
     var addExpense by mutableStateOf(false)
+    var incomeList by mutableStateOf(emptyList<Expense>())
+    var expenseList by mutableStateOf(emptyList<Expense>())
 }
