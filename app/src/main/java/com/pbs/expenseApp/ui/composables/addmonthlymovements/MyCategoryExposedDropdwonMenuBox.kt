8b10139eeb4896d140d3f01ea3549d506a534e3d
Pablo Bascuñana <pablo.bascunana@innocv.com>
2023-12-25 18:57:45 +0100

    PR-23 Implement category select

---
+++ app/src/main/java/com/pbs/expenseApp/ui/composables/addmonthlymovements/MyCategoryExposedDropdwonMenuBox.kt
@@ -0,0 +1,61 @@
+package com.pbs.expenseApp.ui.composables.addmonthlymovements
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.pbs.expenseApp.R
+import com.pbs.expenseApp.ui.AppViewModelProvider
+import com.pbs.expenseApp.ui.components.AppText
+import com.pbs.expenseApp.ui.components.AppTextField
+import com.pbs.expenseApp.ui.viewmodels.CategoryViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyCategoryExposedDropdownMenuBox() {
+    val categoryVM: CategoryViewModel = viewModel(factory = AppViewModelProvider.Factory)
+
+    ExposedDropdownMenuBox(
+        expanded = categoryVM.expandedCategoryDropDown,
+        modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_sm)),
+        onExpandedChange = {
+            categoryVM.expandedCategoryDropDown = !categoryVM.expandedCategoryDropDown
+        },
+    ) {
+        AppTextField(
+            text = stringResource(id = R.string.configuration_category_title),
+            modifier = Modifier
+                .fillMaxWidth()
+                .menuAnchor(),
+            value = categoryVM.categoryName,
+            onValueChange = { categoryVM.categoryName = it },
+            readOnly = true,
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(
+                    expanded = categoryVM.expandedCategoryDropDown
+                )},
+        )
+        ExposedDropdownMenu(
+            expanded = categoryVM.expandedCategoryDropDown,
+            onDismissRequest = { categoryVM.expandedCategoryDropDown = false }
+        ) {
+            categoryVM.categories.forEach { categoryType ->
+                DropdownMenuItem(
+                    text = { AppText(text = categoryType.name) },
+                    onClick = {
+                        categoryVM.categoryName = categoryType.name
+                        categoryVM.expandedCategoryDropDown = false
+                    },
+                    contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                )
+            }
+        }
+    }
+}
