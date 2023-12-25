a26f8a46c71e06ea0e186b573920c6ef9f6c8ab2
Pablo Bascuñana <pablo.bascunana@innocv.com>
2023-12-25 13:46:24 +0100

    PR-23 Add texts to fab buttons

---
+++ app/src/main/java/com/pbs/expenseApp/ui/composables/MyAddExpenseFab.kt
@@ -0,0 +1,39 @@
+package com.pbs.expenseApp.ui.composables
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.pbs.expenseApp.R
+import com.pbs.expenseApp.ui.AppViewModelProvider
+import com.pbs.expenseApp.ui.components.AppIcon
+import com.pbs.expenseApp.ui.components.AppRow
+import com.pbs.expenseApp.ui.components.AppText
+import com.pbs.expenseApp.ui.viewmodels.ExpenseViewModel
+
+@Composable
+fun MyAddExpenseFab() {
+    val expenseVM: ExpenseViewModel = viewModel(factory = AppViewModelProvider.Factory)
+    FloatingActionButton(
+        onClick = {
+            expenseVM.addExpense = !expenseVM.addExpense
+        },
+    ) {
+        AppRow(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_xs))
+        ) {
+            AppIcon(
+                imageVector = Icons.Filled.Add,
+                contentDescription = stringResource(id = R.string.configuration_add_category)
+            )
+            AppText(
+                text = stringResource(id = R.string.add)
+            )
+        }
+    }
+}
