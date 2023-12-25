6a6549fc337a5bb49e1de61fa0f904d68e328fc5
Pablo Bascuñana <pablo.bascunana@innocv.com>
2023-12-25 13:46:00 +0100

    PR-23 Check current route to show appropriate fab button

--- app/src/main/java/com/pbs/expenseApp/ui/components/AppScaffold.kt
+++ app/src/main/java/com/pbs/expenseApp/ui/components/AppScaffold.kt
@@ -4,16 +4,23 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import com.pbs.expenseApp.appstate.rememberAppState
 import com.pbs.expenseApp.navigation.AppNavigationGraph
+import com.pbs.expenseApp.navigation.AppRoutes
+import com.pbs.expenseApp.navigation.BottomBarRoutes
 import com.pbs.expenseApp.ui.composables.MyAddCategoryFab
+import com.pbs.expenseApp.ui.composables.MyAddExpenseFab
 import com.pbs.expenseApp.ui.composables.MyAppNavigationBar
 
 @Composable
 fun AppScaffold() {
     val appState = rememberAppState()
+    val currentRoute = appState.currentRoute
     Scaffold(
         floatingActionButton = {
             if (appState.shouldShowFloatingActionButton) {
-                MyAddCategoryFab()
+                when (currentRoute) {
+                    BottomBarRoutes.CONFIGURATION.route -> MyAddCategoryFab()
+                    else -> MyAddExpenseFab()
+                }
             }
         },
         bottomBar = {
